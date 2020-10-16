d1069d6b0adc07c23e9ec1e61c3762253d1a41a6
Michael Job Figueroa <micjobfigueroa@gmail.com>
2020-10-16 13:33:49 +0800

    Updates

---
+++ precache-manifest.0bc0c1fbd6835798ccb9278bdc92fd1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c083f782cb6225c49780abd1e57773",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "216bee4321dc647716fb",
+    "url": "/react-scoreboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c98f18584e305244ab3",
+    "url": "/react-scoreboard/static/js/2.023e70c1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-scoreboard/static/js/2.023e70c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216bee4321dc647716fb",
+    "url": "/react-scoreboard/static/js/main.126aecf4.chunk.js"
+  },
+  {
+    "revision": "b608536ad2aa96537327",
+    "url": "/react-scoreboard/static/js/runtime-main.a01301ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-scoreboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
